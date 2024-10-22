0137295c4a3db790995decc228e3cd00956ecdc4
SmokeyStack <seansyahril04@gmail.com>
2024-10-22 13:17:37 -0400

    Moved to countdown/momentjs

--- docs/.vitepress/components/days-took.vue
+++ docs/.vitepress/components/days-took.vue
@@ -1,13 +1,7 @@
 <template>
     <h2 v-html="title_modified"></h2>
     <h1 :style="{ color: textColor, fontFamily: 'Mojangles' }">
-        <template v-if="years > 0"> {{ years }} years </template>
-        <template v-if="months > 0"> {{ months }} months </template>
-        <template v-if="days > 0"> {{ days }} days </template>
-        <template v-if="hours > 0"> {{ hours }} hours </template>
-        <template v-if="seconds == 0"> and </template>
-        <template v-if="minutes > 0"> {{ minutes }} minutes </template>
-        <template v-if="seconds > 0"> and {{ seconds }} seconds </template>
+        {{ timeDifference }}
     </h1>
 </template>
 
@@ -24,24 +18,13 @@
 </script>
 
 <script>
+    import moment from 'moment';
+    import countdown from 'countdown';
+
     export default {
         data() {
             return {
-                interval: null,
-                years: 0,
-                months: 0,
-                days: 0,
-                hours: 0,
-                minutes: 0,
-                seconds: 0,
-                intervals: {
-                    second: 1000,
-                    minute: 1000 * 60,
-                    hour: 1000 * 60 * 60,
-                    day: 1000 * 60 * 60 * 24,
-                    month: 1000 * 60 * 60 * 24 * 30,
-                    year: 1000 * 60 * 60 * 24 * 365
-                },
+                timeDifference: 0,
                 title_modified: `${this.title} <span class="VPBadge info">${this.category}</span>`
             };
         },
@@ -72,20 +55,16 @@
         methods: {
             updateDiffs() {
                 //lets figure out our diffs
-                let diff = Math.abs(
-                    this.implemented.getTime() - this.introduced.getTime()
+                this.timeDifference = countdown(
+                    moment(new Date(this.implemented)).toDate(),
+                    moment(new Date(this.introduced)).toDate(),
+                    countdown.YEARS |
+                        countdown.MONTHS |
+                        countdown.DAYS |
+                        countdown.HOURS |
+                        countdown.MINUTES |
+                        countdown.SECONDS
                 );
-                this.years = Math.floor(diff / this.intervals.year);
-                diff -= this.years * this.intervals.year;
-                this.months = Math.floor(diff / this.intervals.month);
-                diff -= this.months * this.intervals.month;
-                this.days = Math.floor(diff / this.intervals.day);
-                diff -= this.days * this.intervals.day;
-                this.hours = Math.floor(diff / this.intervals.hour);
-                diff -= this.hours * this.intervals.hour;
-                this.minutes = Math.floor(diff / this.intervals.minute);
-                diff -= this.minutes * this.intervals.minute;
-                this.seconds = Math.floor(diff / this.intervals.second);
             }
         }
     };
